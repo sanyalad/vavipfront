0bc6884b949445c6406227f1a61adf573e3aab56
Russian Jazz Service <admin@russianjazz.ru>
2025-12-23 22:10:06 +0300

    Fix: export GestureUpdateState type and fix imports

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,10 +5,7 @@ import VideoSection from '@/components/animations/VideoSection'
 import Footer from '@/components/layout/Footer'
 import { useGesturePhysics, calculateWheelRange } from '@/hooks/useGesturePhysics'
 import type { GestureState, GestureDirection } from '@/utils/gesturePhysics'
-import { useTrackpadGesture } from '@/hooks/useTrackpadGesture'
-import type { UseTrackpadGestureOptions } from '@/hooks/useTrackpadGesture'
-
-type GestureUpdateState = Parameters<UseTrackpadGestureOptions['onGestureUpdate']>[0]
+import { useTrackpadGesture, type GestureUpdateState } from '@/hooks/useTrackpadGesture'
 import styles from './Home.module.css'
 
 // ===== DEBUG MODE =====
